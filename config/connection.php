29f30d44be685e06581edcb79601de1eff2fc273
Gody19 <163418534+Gody19@users.noreply.github.com>
2026-02-11 13:01:49 +0300

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,8 +1,14 @@
 <?php
-date_default_timezone_set('Africa/Dar_es_Salaam');
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "food_delivery";
-$conn = new mysqli($hostname, $username, $password, $database);
+$db = parse_url(getenv("DATABASE_URL"));
+
+$conn = new mysqli(
+    $db["host"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
 ?>
